09ff49ae286fbde048759a45aaf4dc719ea7d3a1
Nazmulhasan032 <nh30898@gmail.com>
2023-06-08 10:49:42 +0600

    mongodb server username and pass update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 
@@ -16,8 +17,13 @@ app.use(express.json());
 
 
 
+app.get('/', (req, res)=>{
+    res.send('server is running');
+})
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.5dmbeep.mongodb.net/?retryWrites=true&w=majority";
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5dmbeep.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -46,11 +52,6 @@ run().catch(console.dir);
 
 
 
-
-
-app.get('/', (req, res)=>{
-    res.send('server is running');
-})
 
 
 
