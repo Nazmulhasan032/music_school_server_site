a3c4bfbe6cc98e0ec4f595c82125c0765672faa8
Nazmulhasan032 <nh30898@gmail.com>
2023-06-10 21:05:28 +0600

    enroll class api added

--- index.js
+++ index.js
@@ -13,6 +13,25 @@ app.use(cors());
 app.use(express.json());
 
 
+
+// const verifyJWT = (req, res, next) => {
+//   const authorization = req.headers.authorization;
+//   if (!authorization) {
+//     return res.status(401).send({ error: true, message: 'unauthorized access' });
+//   }
+//   // bearer token
+//   const token = authorization.split(' ')[1];
+
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({ error: true, message: 'unauthorized access' })
+//     }
+//     req.decoded = decoded;
+//     next();
+//   })
+// }
+
+
 app.get('/', (req, res)=>{
     res.send('server is running');
 })
@@ -37,7 +56,10 @@ async function run() {
 
     const usersCollection = client.db("academy").collection("users");
     const classesCollection = client.db("academy").collection("classes");
+    const allClassCollection = client.db("academy").collection("allclass");
     const instructorCollection = client.db("academy").collection("instructor");
+    const allInstructorCollection = client.db("academy").collection("allinstructor");
+    const enrollCollection = client.db("academy").collection("enroll");
 
 
 
@@ -84,6 +106,13 @@ async function run() {
 
     })
 
+// student enroll class saved api-------
+    app.post('/enroll', async(req,res)=>{
+      const enrollClass = req.body;
+      const result = await enrollCollection.insertOne(enrollClass);
+      res.send(result);
+    })
+
 
 
 
@@ -93,8 +122,19 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/allclass', async(req, res)=>{
+        const result = await allClassCollection.find().toArray();
+        res.send(result);
+    })
+
     app.get('/instructor', async(req, res)=>{
         const result = await instructorCollection.find().toArray();
+        res.send(result);
+    })
+
+
+    app.get('/allinstructor', async(req, res)=>{
+        const result = await allInstructorCollection.find().toArray();
         res.send(result);
     })
 
